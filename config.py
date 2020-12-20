9ceb266862e4967bf732d5578c8869e851693fc6
ioneliabuzatu <buzatuionelia@gmail.com>
2020-12-20 21:19:53 +0100

    [CLUSTER] a

--- config.py
+++ config.py
@@ -24,10 +24,10 @@ learning_rate = jax.experimental.optimizers.inverse_time_decay(initial_lr, decay
 eval_every = math.ceil(num_epochs / 1000)
 
 # for remote
-# os.environ["hostname"] = "buzatu@hawk.ml.jku.at"
-# os.environ["password"] =  "LZ46GyzX"
-# os.environ["BUDDY_PORT"] = "5792"
-os.environ["BUDDY_HOSTNAME"] = "root@1159.69.11.199"
-os.environ["BUDDY_PASSWORD"] = "0oKG1L2sz0lVqNe1xo82HQLGc3ah"
+os.environ["hostname"] = "buzatu@hawk.ml.jku.at"
+os.environ["password"] =  "LZ46GyzX"
+os.environ["BUDDY_PORT"] = "5792"
+# os.environ["BUDDY_HOSTNAME"] = "root@159.69.11.199"
+# os.environ["BUDDY_PASSWORD"] = "0oKG1L2sz0lVqNe1xo82HQLGc3ah"
 
 tensorboard = experiment_buddy.deploy(use_remote=True, sweep_yaml="")
