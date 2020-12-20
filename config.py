9b7af1e57731eabc186814417eaaf6f7b0ef422c
ioneliabuzatu <buzatuionelia@gmail.com>
2020-12-20 21:19:15 +0100

    [CLUSTER] a

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ eval_every = math.ceil(num_epochs / 1000)
 # os.environ["hostname"] = "buzatu@hawk.ml.jku.at"
 # os.environ["password"] =  "LZ46GyzX"
 # os.environ["BUDDY_PORT"] = "5792"
-os.environ["BUDDY_HOSTNAME"] = "root@159.69.11.199"
+os.environ["BUDDY_HOSTNAME"] = "root@1159.69.11.199"
 os.environ["BUDDY_PASSWORD"] = "0oKG1L2sz0lVqNe1xo82HQLGc3ah"
 
 tensorboard = experiment_buddy.deploy(use_remote=True, sweep_yaml="")
