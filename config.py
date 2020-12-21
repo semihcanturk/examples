d0a5213d53fb78febfa8f1ebe4b8e1d3d1bb7521
ioneliabuzatu <buzatuionelia@gmail.com>
2020-12-21 16:31:53 +0100

    [CLUSTER] dummy

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import math
 import jax.experimental.optimizers
 
 import experiment_buddy
+import os
 
 initial_lr = .0001
 
@@ -23,5 +24,16 @@ experiment_buddy.register(locals())
 ################################################################
 learning_rate = jax.experimental.optimizers.inverse_time_decay(initial_lr, decay_steps, decay_factor, staircase=True)
 eval_every = math.ceil(num_epochs / 1000)
+
+# for remote
+# user: buzatu
+# os.environ["password"] =  "LZ46GyzX"
+# ssh-keygen
+# or if your server uses custom port no:
+# ssh-copy-id "buzatu@hawk.ml.jku.at -p
+# os.environ["port"] = "5792"
+# os.environ["hostname"] = "buzatu@hawk.ml.jku.at"
+os.environ["BUDDY_HOSTNAME"] = "root@159.69.11.199"
+os.environ["BUDDY_PASSWORD"] = "0oKG1L2sz0lVqNe1xo82HQLGc3ah"
 
 tensorboard = experiment_buddy.deploy(use_remote=True, sweep_yaml="")
