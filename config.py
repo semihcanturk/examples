975e0eef9a0d38efdbedf490260262ae4640e599
manuel <manuel.delverme@gmail.com>
2021-06-16 22:06:49 +0200

    back to ENV variable

--- config.py
+++ config.py
@@ -25,4 +25,4 @@ experiment_buddy.register(locals())
 learning_rate = jax.experimental.optimizers.inverse_time_decay(initial_lr, decay_steps, decay_factor, staircase=True)
 eval_every = math.ceil(num_epochs / 1000)
 
-tensorboard = experiment_buddy.deploy(host=os.environ['BUDDY_HOST'], sweep_yaml="")
+tensorboard = experiment_buddy.deploy(host=os.environ.get('BUDDY_HOST', ""), sweep_yaml="")
